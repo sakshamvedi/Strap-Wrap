f55073b53e6d67f8eb920d3f59159d87ec38470a
sakshamvedi <sakshamvedi@gmail.com>
2022-11-03 16:34:38 +0530

    main

--- script.js
+++ script.js
@@ -9,21 +9,42 @@ function closeNav()
 }
 
 
+function submit()
+{
+    
+    if(document.getElementById("form_data1").value != document.getElementById("form_data2").value )
+    {
+       document.getElementById("straps").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("straps").style.display = "none";
+    }
+
+}
+
+
+
 function alerts()
 {
     document.getElementById("item1").style.display = "block";
     document.getElementById("item2").style.display = "none";
+    document.getElementById("item3").style.display = "none";
+document.getElementById("item4").style.display = "none";
 }
 function crumbs()
 {
     document.getElementById("item1").style.display = "none";
     document.getElementById("item2").style.display = "block";
+    document.getElementById("item3").style.display = "none";
+document.getElementById("item4").style.display = "none";
 }
 function buttons()
 {
     document.getElementById("item1").style.display = "none";
     document.getElementById("item2").style.display = "none";
     document.getElementById("item3").style.display = "block";
+    document.getElementById("item4").style.display = "none";
 
     
 
@@ -35,4 +56,40 @@ function navbar()
 document.getElementById("item2").style.display = "none";
 document.getElementById("item3").style.display = "none";
 document.getElementById("item4").style.display = "block";
+
+}
+
+
+function formly()
+{
+    document.getElementById("item1").style.display = "none";
+    document.getElementById("item2").style.display = "none";
+    document.getElementById("item3").style.display = "none";
+    document.getElementById("item4").style.display = "none";
+    document.getElementById("item5").style.display = "block";
+    document.getElementById("item6").style.display = "none";
+
+}
+
+function card()
+{
+    document.getElementById("item1").style.display = "none";
+    document.getElementById("item2").style.display = "none";
+    document.getElementById("item3").style.display = "none";
+    document.getElementById("item4").style.display = "none";
+    document.getElementById("item5").style.display = "none";
+    document.getElementById("item6").style.display = "block";
+}
+
+
+
+function avatars()
+{
+    document.getElementById("item1").style.display = "none";
+    document.getElementById("item2").style.display = "none";
+    document.getElementById("item3").style.display = "none";
+    document.getElementById("item4").style.display = "none";
+    document.getElementById("item5").style.display = "none";
+    document.getElementById("item6").style.display = "none";
+    document.getElementById("item7").style.display = "block";
 }
